35050c6fb2f679816d1bbffee6f00dc9de17ab9d
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-01-18 20:37:35 +0100

    Ordner strukturiert, navigationslinie berechnet
    Linien renderer funktioniert (nicht)?

---
+++ Assets/SLAM/Scripts/NavMeshPathScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshPathScript : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshPath path;
+    public LineRenderer line;
+    private float elapsed = 0.0f;
+    void Start()
+    {
+        Instantiate(line);
+        
+        this.path = new NavMeshPath();
+        elapsed = 0.0f;
+    }
+
+    void Update()
+    {
+        // Update the way to the goal every second.
+        elapsed += Time.deltaTime;
+        if (elapsed > 1.0f)
+        {
+            elapsed -= 1.0f;
+            NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+        }
+        for (int i = 0; i < path.corners.Length - 1; i++)
+        {
+            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+            
+        }
+        Debug.Log(path.corners);
+        line.SetPositions(path.corners);
+        
+    }
+}
