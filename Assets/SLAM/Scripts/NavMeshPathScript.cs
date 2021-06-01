b83837c996a71e9cf1442a9f29251d4ee87a9ad1
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-06-01 14:56:34 +0200

    Es passieren Sachen
    Atome... überall atome
    Aber wenigstens sind welche da...?
    Diagonal ist schwer, vllt geht das so gar nicht
    Sonst... sucess?

--- Assets/SLAM/Scripts/NavMeshPathScript.cs
+++ Assets/SLAM/Scripts/NavMeshPathScript.cs
@@ -10,8 +10,10 @@ public class NavMeshPathScript : MonoBehaviour
     public LineRenderer line;
     public Material pathMaterial;
     private float elapsed = 0.0f;
+    public GameObject punkt;
+    public Vector3 hinlegerVektor;
     void Start()
-    {
+    {              
         Instantiate(line);
         line.textureMode = LineTextureMode.Tile;
         line.GetComponent<LineRenderer>().material = pathMaterial;
@@ -21,28 +23,62 @@ public class NavMeshPathScript : MonoBehaviour
         target.position = new Vector3(TargetPossition.possition[0], target.position.y, TargetPossition.possition[1]);
         Debug.Log("Start");
         Debug.Log(target.position);
+
+
+        hinlegerVektor = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Debug.Log(hinlegerVektor);
     }
 
     void Update()
     {
-
-
         // Update the way to the goal every second.
         elapsed += Time.deltaTime;
         if (elapsed > 1.0f)
         {
             elapsed -= 1.0f;
-
-
+            
             NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+            hinlegen();
         }
-        for (int i = 0; i < path.corners.Length - 1; i++)
-        {
-            //Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
-
-        }
+        
         line.positionCount = path.corners.Length;
         line.SetPositions(path.corners);
+        Debug.Log(path.corners.Length);
 
+        
+                     
+    }
+
+    void hinlegen()
+    {
+        
+        for (int i = 0; i <= path.corners.Length-1; i++)
+        {
+            Debug.Log(path.corners[i]);
+            while (hinlegerVektor.x + 2 < path.corners[i].x)
+            {
+                hinlegerVektor.x = hinlegerVektor.x + 2f;
+                Instantiate(punkt, hinlegerVektor, target.rotation);
+            }
+
+            while (hinlegerVektor.z + 2 < path.corners[i].z)
+            {
+
+                hinlegerVektor.z = hinlegerVektor.z + 2f;
+                Instantiate(punkt, hinlegerVektor, target.rotation);
+            }
+
+            while (hinlegerVektor.x - 2 > path.corners[i].x)
+            {
+                hinlegerVektor.x = hinlegerVektor.x - 2f;
+                Instantiate(punkt, hinlegerVektor, target.rotation);
+            }
+
+            while (hinlegerVektor.z - 2 > path.corners[i].z)
+            {
+                hinlegerVektor.z = hinlegerVektor.z - 2f;
+                Instantiate(punkt, hinlegerVektor, target.rotation);
+            }
+        }
     }
 }
