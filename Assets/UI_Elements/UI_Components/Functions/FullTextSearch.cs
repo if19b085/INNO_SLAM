0cebf3a8da19c3e5dc3bb2dfd9933bcd25156263
Thomas Pleiner <if19b085@technikum-wien.at>
2021-06-26 19:51:21 +0200

    search functionality fixed

--- Assets/UI_Elements/UI_Components/Functions/FullTextSearch.cs
+++ Assets/UI_Elements/UI_Components/Functions/FullTextSearch.cs
@@ -11,11 +11,11 @@ using Newtonsoft.Json.Linq;
 
 public class FullTextSearch : MonoBehaviour
 {
+    public GameObject ListElementPrefab;
     public GameObject Content;
-    public Font Font_Of_List_Element;
     public string Search_Term;
-    private List<string> list = new List<string>();
     private List<Room> roomList = new List<Room>();
+    List<Room> displayList;
     private Room roomy;
     private InputField input;
 
@@ -26,53 +26,36 @@ public class FullTextSearch : MonoBehaviour
     void Start()
     {
         JsonParse();
-
         input = gameObject.GetComponent<InputField>();
         input.onValueChanged.AddListener(delegate { SubmitName(); });
-        //var se = new InputField.SubmitEvent();
-        //se.AddListener(SubmitName);
-        //input.onEndEdit = se;
-
     }
      
     public void SubmitName()
     {
-        JsonParse();
-        Search_Term = input.text;
-
-
-        //Create List of Searchable Items 
-        //Application.dataPath to relatively search file
         
+        foreach (Transform child in Content.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+       
+        Search_Term = input.text;
 
 
         if (!String.IsNullOrEmpty(Search_Term))
         {
-            roomList = roomList.Where(x => x.id.ToLower().Contains(Search_Term.ToLower())).ToList();
+            displayList = roomList.Where(x => x.id.ToLower().Contains(Search_Term.ToLower())).ToList();
         }
 
-        foreach (Room listElement in roomList)
+        foreach (Room listElement in displayList)
         {
-            //We call a new Game Object named listItem
-            GameObject ngo = new GameObject();
-            //Now we set the to-be parent object of this Game Object
-            //Prefabs generieren und eine Liste von GameObject zu erstellen -> über OnValueChange(look at instantiate)
-            ngo.transform.SetParent(Content.transform);
-            //By adding a text Component to the new Game Objekt it makes it equivalent to the UI template relative
-            Text myText = ngo.AddComponent<Text>();
-            //Now we fill this text Component with the wished text
-            myText.text = listElement.id;
-            myText.font = Font_Of_List_Element;
-            myText.fontStyle = FontStyle.Bold;
-            myText.fontSize = 35;
-            myText.color = Color.gray;
+            ListElementPrefab.GetComponent<UnityEngine.UI.Text>().text = listElement.id;
+            Instantiate(ListElementPrefab, Content.transform);
         }
-
     }
 
     public void JsonParse()
     {
-        string jsonString = File.ReadAllText("Assets/UI_Elements/UI_Components/JSON/fhtwrooms.json");
+        string jsonString = File.ReadAllText("Assets/UI_Elements/UI_Components/JSON/fhtwroomsF_E.json");
         JObject data = JObject.Parse(jsonString);
         string roomArray = data["roomDescriptions"].ToString();
         JArray jArray = JArray.Parse(roomArray);
