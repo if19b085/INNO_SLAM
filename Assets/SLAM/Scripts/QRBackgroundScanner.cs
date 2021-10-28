93cf081b2ae3c52d9523e73f85b94b051fa3c632
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-10-28 13:21:03 +0200

    Qr Background Scanner
    Weeeeeeeee

--- Assets/SLAM/Scripts/QRBackgroundScanner.cs
+++ Assets/SLAM/Scripts/QRBackgroundScanner.cs
@@ -2,24 +2,25 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 using ZXing;
 
 public class QRBackgroundScanner : MonoBehaviour
 {
 
+    public ARCameraManager cameraManager;
 
-    WebCamTexture webcamTexture;
     string QrCode = string.Empty;
-    public AudioSource beepSound;
-    public Text text;
+    
+    
 
     void Start()
     {
-        var renderer = GetComponent<RawImage>();
-        webcamTexture = new WebCamTexture(512, 512);
-        renderer.material.mainTexture = webcamTexture;
-        webcamTexture.Play();
-        text.gameObject.SetActive(false);
+
+
+        var renderer = GetComponent<RawImage>();      
+       
         StartCoroutine(GetQRCode());
         InvokeRepeating(nameof(GetAnotherQrCode), 2f, 2f);
     }
@@ -27,38 +28,52 @@ public class QRBackgroundScanner : MonoBehaviour
 
     IEnumerator GetQRCode()
     {
-        Debug.Log("i bims qrcodegetter");
-        IBarcodeReader barCodeReader = new BarcodeReader();
-        var snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
+
 
         try
         {
-            snap.SetPixels32(webcamTexture.GetPixels32());
-            var Result = barCodeReader.Decode(snap.GetRawTextureData(), webcamTexture.width, webcamTexture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
-            if (Result != null)
+            if (cameraManager.TryAcquireLatestCpuImage(out XRCpuImage cpuImage))
             {
-                QrCode = Result.Text;
-                if (!string.IsNullOrEmpty(QrCode))
+                using (cpuImage)
                 {
-                    if (!string.IsNullOrEmpty(QrCode))
-                    {
-                        string[] xAndZ = QrCode.Split('/');
-                        float x = float.Parse(xAndZ[0]);
-                        float z = float.Parse(xAndZ[1]);
-                        SceneDataHandler.myData.startX = x;
-                        SceneDataHandler.myData.startZ = z;
-                        Debug.Log(x + ", " + z);
-                        text.gameObject.SetActive(true);
-                        QrCode = null;
+                    var texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
 
+                    var conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorY);
+                    var rawTextureData = texture.GetRawTextureData<byte>();
+                    cpuImage.Convert(conversionParams, rawTextureData);
+                    texture.Apply();
+
+                    IBarcodeReader barCodeReader = new BarcodeReader();
+                    var Result = barCodeReader.Decode(texture.GetRawTextureData(), texture.width, texture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+
+                    if (Result != null)
+                    {
+                        QrCode = Result.Text;
+                        if (!string.IsNullOrEmpty(QrCode))
+                        {
+                            if (!string.IsNullOrEmpty(QrCode))
+                            {
+                                string[] xAndZ = QrCode.Split('/');
+                                float x = float.Parse(xAndZ[0]);
+                                float z = float.Parse(xAndZ[1]);
+                                SceneDataHandler.myData.startX = x;
+                                SceneDataHandler.myData.startZ = z;
+                                Debug.Log(x + ", " + z);                              
+                                QrCode = null;
+                                
+                            }
+                        }
                     }
                 }
             }
         }
+
+
+
+
+
         catch (Exception ex) { Debug.LogWarning(ex.Message); }
         yield return null;
-
-
 
     }
 
@@ -69,38 +84,57 @@ public class QRBackgroundScanner : MonoBehaviour
 
     void GetAnotherQrCode()
     {
-        Debug.Log("hi");
-        IBarcodeReader barCodeReader = new BarcodeReader();
-        var snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
-
-
         try
         {
-            snap.SetPixels32(webcamTexture.GetPixels32());
-            var Result = barCodeReader.Decode(snap.GetRawTextureData(), webcamTexture.width, webcamTexture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
-            if (Result != null)
+            if (cameraManager.TryAcquireLatestCpuImage(out XRCpuImage cpuImage))
             {
-                QrCode = Result.Text;
-                if (!string.IsNullOrEmpty(QrCode))
+                using (cpuImage)
                 {
-                    if (!string.IsNullOrEmpty(QrCode))
-                    {
-                        string[] xAndZ = QrCode.Split('/');
-                        float x = float.Parse(xAndZ[0]);
-                        float z = float.Parse(xAndZ[1]);
-                        SceneDataHandler.myData.startX = x;
-                        SceneDataHandler.myData.startZ = z;
-                        Debug.Log(x + ", " + z);                        
-                        text.gameObject.SetActive(true);                       
-                        QrCode = null;
+                    var texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
 
+                    var conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorY);
+                    var rawTextureData = texture.GetRawTextureData<byte>();
+                    cpuImage.Convert(conversionParams, rawTextureData);
+                    texture.Apply();
+
+                    IBarcodeReader barCodeReader = new BarcodeReader();
+                    var Result = barCodeReader.Decode(texture.GetRawTextureData(), texture.width, texture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+
+                    if (Result != null)
+                    {
+                        QrCode = Result.Text;
+                        if (!string.IsNullOrEmpty(QrCode))
+                        {
+                            if (!string.IsNullOrEmpty(QrCode))
+                            {
+                                string[] xAndZ = QrCode.Split('/');
+                                float x = float.Parse(xAndZ[0]);
+                                float z = float.Parse(xAndZ[1]);
+                                SceneDataHandler.myData.startX = x;
+                                SceneDataHandler.myData.startZ = z;
+                                Debug.Log(x + ", " + z);
+                                QrCode = null;
+                               
+                            }
+                        }
                     }
                 }
             }
         }
+
+
+
+
+
         catch (Exception ex) { Debug.LogWarning(ex.Message); }
+        
 
     }
+
+
+
+
+
 
     /*
     void Start()
@@ -150,5 +184,40 @@ public class QRBackgroundScanner : MonoBehaviour
                 Debug.Log("RECOGNIZED: " + result.Text);
             }
         }
+
+    // Source: https://github.com/Unity-Technologies/arfoundation-samples/blob/6296272a416925b56ce85470e0c7bef5c913ec0c/Assets/Scripts/CpuImageSample.cs
+    private Texture2D UpdateRawImage(XRCpuImage cpuImage)
+    {
+        // Get the texture associated with the UI.RawImage that we wish to display on screen.
+        var texture = new Texture2D();
+
+        // If the texture hasn't yet been created, or if its dimensions have changed, (re)create the texture.
+        // Note: Although texture dimensions do not normally change frame-to-frame, they can change in response to
+        //    a change in the camera resolution (for camera images) or changes to the quality of the human depth
+        //    and human stencil buffers.
+        if (texture == null || texture.width != cpuImage.width || texture.height != cpuImage.height)
+        {
+            texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
+            rawImage.texture = texture;
+        }
+
+        // For display, we need to mirror about the vertical access.
+        var conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorY);
+
+        //Debug.Log("Texture format: " + cpuImage.format.AsTextureFormat()); -> RFloat
+
+        // Get the Texture2D's underlying pixel buffer.
+        var rawTextureData = texture.GetRawTextureData<byte>();
+
+        // Make sure the destination buffer is large enough to hold the converted data (they should be the same size)
+        Debug.Assert(rawTextureData.Length == cpuImage.GetConvertedDataSize(conversionParams.outputDimensions, conversionParams.outputFormat),
+            "The Texture2D is not the same size as the converted data.");
+
+        // Perform the conversion.
+        cpuImage.Convert(conversionParams, rawTextureData);
+
+        // "Apply" the new pixel data to the Texture2D.
+        texture.Apply();
+    }
     }*/
 }
